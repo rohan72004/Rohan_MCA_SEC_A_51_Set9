670dc9e2c7a8072a5676a155cf3483ced2afe807
Rohan Singh <161342207+rohan72004@users.noreply.github.com>
2026-04-10 10:11:44 +0530

    Update DBConnection.java

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,1 +1,16 @@
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class DBConnection {
+    public static Connection getConnection() {
+        Connection con = null;
+        try {
+            String url = "jdbc:mysql://localhost:3306/gym_locker";
+            String user = "root";
+            String pass = "Yadu@123"; 
+            con = DriverManager.getConnection(url, user, pass);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
