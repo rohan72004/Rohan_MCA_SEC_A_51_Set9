3c0d59660fbc4fe3e6573d82d947b1a14479c6ba
Rohan Singh <161342207+rohan72004@users.noreply.github.com>
2026-04-12 12:43:55 +0530

    Update Schema.sql

--- Sql/Schema.sql
+++ Sql/Schema.sql
@@ -1,13 +1,19 @@
+CREATE DATABASE IF NOT EXISTS gym_locker;
+USE gym_locker;
 CREATE TABLE members (
- member_id INT PRIMARY KEY AUTO_INCREMENT,
- name VARCHAR(100),
- phone VARCHAR(15),
- plan ENUM('monthly','quarterly','annual')
+    member_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    phone VARCHAR(15) NOT NULL UNIQUE,
+    plan VARCHAR(20) NOT NULL
 );
 CREATE TABLE lockers (
- locker_id INT PRIMARY KEY AUTO_INCREMENT,
- locker_no VARCHAR(10) UNIQUE,
- member_id INT DEFAULT NULL,
- assigned_on DATE DEFAULT NULL,
- FOREIGN KEY (member_id) REFERENCES members(member_id)
+    locker_id INT AUTO_INCREMENT PRIMARY KEY,
+    locker_no VARCHAR(10) UNIQUE,
+    member_id INT NULL,
+    assigned_on DATE,
+    FOREIGN KEY (member_id) REFERENCES members(member_id)
 );
+INSERT INTO lockers (locker_no) VALUES
+('L1'),
+('L2'),
+('L3');
